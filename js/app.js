f3ccc33ff36652457195b16f22f2cc6d3f378318
neilw22 <neilw22@gmail.com>
2015-04-27 14:07:19 -0400

    Created array with question content placeholders, some click behaviour and an overlay for answer information

--- js/app.js
+++ js/app.js
@@ -1,49 +1,46 @@
 $(document).ready(function(){
 
-function question(q, opt1, opt2, opt3, opt4){
-	this.q = q;
-	this.opt1 = opt1;
-	this.opt2 = opt2;
-	this.opt3 = opt3;
-	this.opt4 = opt4;
+var nextQ = 2
+
+function answer(x) {
+	if (nextQ === 2) {
+		x = q2;
+		$('.answer').show();
+	} else if (nextQ === 3) {
+		x = q3;
+		$('.answer').show();
+	} else if (nextQ === 4) {
+		x = q4;
+		$('.answer').show();
+	} else if (nextQ === 5) {
+		x = q5;
+		$('.answer').show();
+	} 
+	$("#q-content").text(x[0]);
+	$("#a1-content").text(x[1]);
+	$("#a2-content").text(x[2]);
+	$("#a3-content").text(x[3]);
+	$("#a4-content").text(x[4]);
+	nextQ++;
 }
 
-function answer(){
+var q2 = ["this is question 2" , "q2-opt1" , "q2-opt2" , "q2-opt3" , "q2-opt4"];
+var q3 = ["this is question 3" , "q3-opt1" , "q3-opt2" , "q3-opt3" , "q3-opt4"];
+var q4 = ["this is question 4" , "q4-opt1" , "q4-opt2" , "q4-opt3" , "q4-opt4"];
+var q5 = ["this is question 5" , "q5-opt1" , "q5-opt2" , "q5-opt3" , "q5-opt4"];
 
-	$(".question-box").text(q2.q);
-	$("#answer1").text(q2.opt1);
-	$("#answer2").text(q2.opt2);
-	$("#answer3").text(q2.opt3);
-	$("#answer4").text(q2.opt4);
-	$("#question-number").text("2");
-
-}
-
-var q2 = new question("this is question 2", "q2-opt1", "q2-opt2", "q2-opt3", "q2-opt4");
-var q3 = new question("this is question 3", "q3-opt1", "q3-opt2", "q3-opt3", "q3-opt4");
-var q4 = new question("this is question 4", "q4-opt1", "q4-opt2", "q4-opt3", "q4-opt4");
-var q5 = new question("this is question 5", "q5-opt1", "q5-opt2", "q5-opt3", "q5-opt4");
-
-
-$("#answer1").click(function(){
+$(".a").click(function(){
+	if (document.getElementById('answer1').onclick==true) {
+		$("#answer-correct").show();
+	} else {
+		$("answer-incorrect").show();
+	}
 	answer();
 });
 
-$("#answer2").click(function(){
-	answer();
-});
-
-$("#answer3").click(function(){
-	answer();
-});
-
-$("#answer4").click(function(){
-	answer();
-});
-
-
-
-
+$("#answer-closer").click(function(){
+	$('.answer').hide();
+})
 
 
 
